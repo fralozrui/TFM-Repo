49ac04995b733684253e0fc575119c0aa54e9302
fralozrui <fran13loru@gmail.com>
2025-07-14 18:23:14 +0200

    .gitignore

---
+++ Nodes/object_localizer.py
@@ -0,0 +1,5 @@
+"""
+Nodo responsable de detectar y localizar objetos específicos en imágenes,
+utilizando modelos como YOLOv8 o Grounding DINO. 
+Permite responder preguntas como “¿dónde está el mando?” o “¿ves un enchufe?”.
+"""
---
+++ Nodes/scene_description.py
@@ -0,0 +1,5 @@
+"""
+Nodo especializado en la descripción automática de escenas a partir de imágenes.
+Utiliza modelos preentrenados como BLIP, GIT o similares para generar descripciones 
+semánticas útiles.
+"""
