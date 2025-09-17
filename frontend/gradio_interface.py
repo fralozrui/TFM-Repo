c31225820f6632e325aeb9cc5199e1824bf27f05
Francisco José Lozano Ruiz <francisco.lozano.ext@sener.es>
2025-09-17 19:58:01 +0200

    Fix frontend_requirements y show_api False

--- frontend/gradio_interface.py
+++ frontend/gradio_interface.py
@@ -158,4 +158,4 @@ with gr.Blocks() as demo:
 
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 8080))
-    demo.launch(server_name="0.0.0.0", server_port=port, share=False, inbrowser=False)
+    demo.launch(server_name="0.0.0.0", server_port=port, share=False, inbrowser=False, show_api=False)
